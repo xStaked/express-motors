705fe9f87bca64041d8b2780515b3bccc31711e9
= <=>
2023-06-03 20:31:09 -0500

    feat: add env config

--- src/database/config.js
+++ src/database/config.js
@@ -2,10 +2,10 @@ const { Sequelize } = require('sequelize');
 
 const db = new Sequelize({
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: '123',
-    database: 'motors_db',
+    host: process.env.DB_HOST || 'localhost',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     logging: false,
 });
 
